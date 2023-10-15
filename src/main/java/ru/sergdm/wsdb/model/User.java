c800b837f35dd088e465ab0b720bc2c187d312cf
sergdm <sergey.dmitr@mail.ru>
2023-10-15 18:32:10 +0300

    Make username unique constraint

--- src/main/java/ru/sergdm/wsdb/model/User.java
+++ src/main/java/ru/sergdm/wsdb/model/User.java
@@ -1,5 +1,6 @@
 package ru.sergdm.wsdb.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -13,7 +14,8 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
+	@Column(name = "username" ,unique=true,nullable = false)
 	private String username;
 	
 	private String firstName;
